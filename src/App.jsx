09b3c799aa10e439758b817fbb6ce37e64af2804
FarrukhS <909tjk@gmail.com>
2024-10-20 23:35:09 -0500

    added about and mission sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import HeroSection from "./components/HeroSection"
 import Navbar from "./components/Navbar"
 import Dishes from "./components/Dishes"
+import About from "./components/About"
+import Mission from "./components/Mission"
 
 const App = () => {
   return (
@@ -8,6 +10,8 @@ const App = () => {
       <HeroSection />
       <Navbar />
       <Dishes />
+      <About />
+      <Mission />
     </main>
   )
 }
